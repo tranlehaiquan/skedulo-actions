4463c688046860676236f91503bfc5359ab2ee7d
Linh Tran <linh.tran@skedulo.com>
2021-12-09 16:13:07 +0700

    remove sample page

--- main-pkg/demoConnectedPage/src/commons/constants.ts
+++
@@ -1,9 +0,0 @@
-export enum TAB_ROUTES {
-  OVERVIEW = 'overview',
-  RESOURCES = 'resources',
-}
-
-export const TAB_OPTIONS = [
-  { route: TAB_ROUTES.OVERVIEW, title: 'Overview' },
-  { route: TAB_ROUTES.RESOURCES, title: 'Resources' },
-]
--- main-pkg/demoConnectedPage/src/services/constants.ts
+++
@@ -1,6 +0,0 @@
-export const END_POINTS = {
-  GRAPHQL: '/graphql/graphql',
-  NOTIFICATION_ONE_OFF: '/notifications/oneoff',
-  AVAILABILITY_RESOURCES: '/availability/resources',
-  AVAILABILITY_SIMPLE: '/availability/simple',
-}
