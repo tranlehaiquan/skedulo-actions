69746890d7ed888d5394eac3aafe529aa6d3d9c2
tranlehaiquan <tranlehaiquan@gmail.com>
2023-03-26 15:54:17 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,40 @@
-import { PackageService, AuthorizeData } from './PackageService'
-import * as dotenv from 'dotenv'
+import { PackageService, AuthorizeData } from "./PackageService";
+import * as dotenv from "dotenv";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-dotenv.config()
+dotenv.config();
 
 async function deployedPackages() {
-  const packagePaths = process.env.PACKAGE_PATH ? process.env.PACKAGE_PATH.split(",") : ['main-pkg']
+  const packagePath =
+    process.env.PACKAGE_PATH || core.getInput("PACKAGE_PATH") || "main-pkg";
+  const packagePaths = packagePath.split(",");
 
   const authorizeData: AuthorizeData = {
-    token: process.env.SKEDULO_API_TOKEN || '',
-    API_SERVER: process.env.SKEDULO_API_SERVER || 'https://api.skedulo.com/',
-    ORG_NAME: process.env.ORG_NAME || 'Testing'
-  }
-
+    token:
+      process.env.SKEDULO_API_TOKEN || core.getInput("SKEDULO_API_TOKEN") || "",
+    API_SERVER:
+      process.env.SKEDULO_API_SERVER ||
+      core.getInput("SKEDULO_API_SERVER") ||
+      "https://api.skedulo.com/",
+    ORG_NAME: process.env.ORG_NAME || core.getInput("ORG_NAME") || "Testing",
+  };
 
   await Promise.all(
     packagePaths.map(async (packagePath) => {
+      const packageService = new PackageService(packagePath, authorizeData);
 
-      const packageService = new PackageService(packagePath, authorizeData)
-
-      return await packageService.deploy((status) => console.log(status))
+      return await packageService.deploy((status) => console.log(status));
     })
-  )
+  );
 }
 
-deployedPackages()
+(async () => {
+  try {
+    const time = new Date().toTimeString();
+    core.setOutput("Start time", time);
+    await deployedPackages();
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+})();
