9152b0d889e4962ae9c231758de6db3c21479638
tranlehaiquan <tranlehaiquan@gmail.com>
2023-03-26 14:49:57 +0700

    little update

--- dist/index.js
+++ dist/index.js
@@ -9796,6 +9796,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
+  console.log(JSON.stringify(process.env));
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
